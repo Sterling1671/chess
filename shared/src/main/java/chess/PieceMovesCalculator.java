926caf4df47ce0dc08cd0be13b6dfff74dbf4122
Sterling Griffin <sterlingelijah@gmail.com>
2026-05-03 22:35:00 -0600

    First implementation of BisopMovesCalculator, added copy menthod to ChessPosition and default method to PieceMovesCalculator. UNTESTED

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -3,12 +3,12 @@ package chess;
 import java.util.Collection;
 
 public interface PieceMovesCalculator {
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
 
     /**
      * @return Returns true if the position is in bounds
      */
-    default public boolean checkInBounds(ChessPosition myPosition){
+    default boolean checkInBounds(ChessPosition myPosition){
         return myPosition.getRow() <= 8 && myPosition.getRow() >= 1 && myPosition.getColumn() <= 8 && myPosition.getColumn() >= 1;
     }
 }
