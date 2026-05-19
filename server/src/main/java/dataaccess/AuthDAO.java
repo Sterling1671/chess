925797a01b9bb46561aac2bbc4b59a767d913a9e
Sterling Griffin <sterlingelijah@gmail.com>
2026-05-19 14:40:53 -0600

    Added methods to the DAO interfaces and added the service classes

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void clear();
+    void createAuth(AuthData authData);
+    AuthData getAuth(String authToken);
+    void deleteAuth(AuthData authData);
 }
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public interface GameDAO {
+    void clear();
+    void createGame(GameData gameData);
+    GameData getGame(int gameID);
+    Collection<GameData> listGames();
+    void updateGame(GameData gameData);
+
 }
