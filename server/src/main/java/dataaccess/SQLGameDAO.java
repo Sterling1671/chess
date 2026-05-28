db68fac71abb7a4da275dbf42dabd8bf84c90301
Sterling Griffin <sterlingelijah@gmail.com>
2026-05-28 01:04:13 -0600

    Fixed some small issues in SQLGameDAO and wrote framework for tests

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -20,11 +20,11 @@ public class SQLGameDAO implements GameDAO{
             """
             CREATE TABLE IF NOT EXISTS  games (
               `id` INT NOT NULL,
-              `whiteUsername` varchar(256) NOT NULL,
-              `blackUsername` varchar(256) NOT NULL,
-              'gameName' varchar(256) NOT NULL,
-              'game' TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
+              `whiteUsername` varchar(256) DEFAULT NULL,
+              `blackUsername` varchar(256) DEFAULT NULL,
+              `gameName` varchar(256) NOT NULL,
+              `game` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -1,6 +1,52 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import org.junit.jupiter.api.*;
+
 public class GameDAOTests {
     private static GameDAO gameDAO;
+    private static GameData existingGame;
+    private static GameData gameHasBlackPlayer;
+    private static GameData gameHasTwoPlayers;
+
+    @BeforeAll
+    public static void init(){
+        try{
+            gameDAO = new SQLGameDAO();
+        } catch (DataAccessException e) {
+            System.err.println("Failed setup");
+        }
+        existingGame = new GameData(
+                1,
+                null,
+                null,
+                "gameHasNoPlayers",
+                new ChessGame());
+        gameHasBlackPlayer = new GameData(
+                1,
+                null,
+                "blackUsername",
+                "gameHasOnePlayer",
+                new ChessGame());
+        gameHasTwoPlayers = new GameData(
+                1,
+                "whiteUsername",
+                "blackUsername",
+                "gameHasTwoPlayers",
+                new ChessGame());
+    }
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        gameDAO.clear();
+        gameDAO.createGame(existingGame);
+    }
+
+    @Test
+    @DisplayName("Check existing game")
+    public void checkExistingGame() throws DataAccessException {
+        Assertions.assertEquals(gameDAO.getGame(existingGame.gameID()),existingGame);
+    }
 
 }
