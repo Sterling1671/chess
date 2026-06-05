906fbdfa810976ed2be93e26c01981f752451a6e
Sterling Griffin <sterlingelijah@gmail.com>
2026-06-04 19:50:39 -0600

    Added framework for tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,12 +7,15 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + String.format("%d",port) + "/");
+
     }
 
     @AfterAll
@@ -22,7 +25,59 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
+    public void loginPos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void loginNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void regPos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void regNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void logoutPos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void logoutNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void createGamePos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void createGameNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void listGamesPos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void listGamesNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void joinGamePos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void joinGameNeg() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void getGamePos() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void getGameNeg() {
         Assertions.assertTrue(true);
     }
 
