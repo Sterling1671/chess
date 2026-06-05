78bc61ae2fab5a266e1d98d149201f92aedbfb0d
Sterling Griffin <sterlingelijah@gmail.com>
2026-06-04 20:32:28 -0600

    added test baseline

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,21 +1,37 @@
 package client;
 
+import model.requests.LoginRequest;
+import model.requests.LogoutRequest;
+import model.requests.RegisterRequest;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.util.Random;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
+    private static final Random random = new Random();
+    private static final String existingUserName = String.format("TestUser_%d",random.nextInt(100000));
+    private static final String newUserName = String.format("TestNewUser_%d",random.nextInt(100000));
 
+    private static final RegisterRequest existingUser = new RegisterRequest(existingUserName,"ePassword","eEmail");
+    private static final LoginRequest existingLogin = new LoginRequest(existingUserName,"ePassword");
+    private static String authToken;
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade("http://localhost:" + String.format("%d",port) + "/");
+        authToken = facade.register(existingUser).authToken();
+    }
 
+    @BeforeEach
+    public void setup(){
+        authToken = facade.login(existingLogin).authToken();
     }
 
     @AfterAll
@@ -26,27 +42,27 @@ public class ServerFacadeTests {
 
     @Test
     public void loginPos() {
-        Assertions.assertTrue(true);
+        Assertions.assertDoesNotThrow(() -> facade.login(existingLogin));
     }
     @Test
     public void loginNeg() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () -> facade.login(new LoginRequest("a","a")));
     }
     @Test
     public void regPos() {
-        Assertions.assertTrue(true);
+        Assertions.assertDoesNotThrow(() -> facade.register(new RegisterRequest(newUserName,"newTestPassword", "newTestEmail")));
     }
     @Test
     public void regNeg() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () -> facade.register(existingUser));
     }
     @Test
     public void logoutPos() {
-        Assertions.assertTrue(true);
+        Assertions.assertDoesNotThrow(() -> facade.logout(new LogoutRequest(authToken)));
     }
     @Test
     public void logoutNeg() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () -> facade.logout(new LogoutRequest("yeet")));
     }
     @Test
     public void createGamePos() {
